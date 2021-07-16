b08a1a644ed31c16b959a899071e1ae8ddd31c0b
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-07-15 22:58:32 -0400

    initial commit. buildkit:0.8.3

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.buildkit
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-buildkit/actions/workflows/build.yml/badge.svg)
+
+Installs [buildkit](https://github.com/moby/buildkit)
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.buildkit
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/moby/buildkit/releases/download
+
+# https://github.com/moby/buildkit/releases/download/v0.8.3/buildkit-v0.8.3.linux-amd64.tar.gz
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local suffix=$4
+    local platform="${os}-${arch}"
+    local file=buildkit-v${ver}.${platform}.${suffix}
+    local dlfile=$DIR/$file
+    local url=$MIRROR/v${ver}/$file
+    if [ ! -e $dlfile ]
+    then
+        wget -q -O $dlfile $url
+    fi
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform `sha256sum $dlfile | awk '{print $1}'`
+}
+
+dl_ver ()
+{
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64 tar.gz
+    dl $ver linux amd64 tar.gz
+    dl $ver linux arm-v7 tar.gz
+    dl $ver linux arm64 tar.gz
+    dl $ver linux ppc64le tar.gz
+    dl $ver linux s390x tar.gz
+    dl $ver windows amd64 zip
+}
+
+dl_ver ${1:-0.8.3}
