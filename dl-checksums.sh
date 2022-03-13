381ada561e50de1630bd2901ff319469463f6140
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-03-12 21:38:45 -0500

    buildkit:0.10.0

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -17,7 +17,7 @@ dl()
     local url=$MIRROR/v${ver}/$file
     if [ ! -e $dlfile ]
     then
-        wget -q -O $dlfile $url
+        curl -sSLf -o $dlfile $url
     fi
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform `sha256sum $dlfile | awk '{print $1}'`
@@ -28,12 +28,15 @@ dl_ver ()
     local ver=$1
     printf "  '%s':\n" $ver
     dl $ver darwin amd64 tar.gz
+    dl $ver darwin arm64 tar.gz
     dl $ver linux amd64 tar.gz
     dl $ver linux arm-v7 tar.gz
     dl $ver linux arm64 tar.gz
     dl $ver linux ppc64le tar.gz
+    dl $ver linux riscv64 tar.gz
     dl $ver linux s390x tar.gz
-    dl $ver windows amd64 zip
+    dl $ver windows amd64 tar.gz
+    dl $ver windows arm64 tar.gz
 }
 
-dl_ver ${1:-0.9.3}
+dl_ver ${1:-0.10.0}
