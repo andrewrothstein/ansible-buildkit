941dfe99e35d07b233aa9c24852ea462ef29f2a3
Andrew Rothstein <andrew.rothstein@gmail.com>
2025-03-06 21:44:44 +0000

    buildkit:0.20.1

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -10,7 +10,7 @@ dl()
     local ver=$1
     local os=$2
     local arch=$3
-    local suffix=$4
+    local suffix=${4:-tar.gz}
     local platform="${os}-${arch}"
     local file=buildkit-v${ver}.${platform}.${suffix}
     local dlfile=$DIR/$file
@@ -27,16 +27,16 @@ dl_ver ()
 {
     local ver=$1
     printf "  '%s':\n" $ver
-    dl $ver darwin amd64 tar.gz
-    dl $ver darwin arm64 tar.gz
-    dl $ver linux amd64 tar.gz
-    dl $ver linux arm-v7 tar.gz
-    dl $ver linux arm64 tar.gz
-    dl $ver linux ppc64le tar.gz
-    dl $ver linux riscv64 tar.gz
-    dl $ver linux s390x tar.gz
-    dl $ver windows amd64 tar.gz
-    dl $ver windows arm64 tar.gz
+    dl $ver darwin amd64
+    dl $ver darwin arm64
+    dl $ver linux amd64
+    dl $ver linux arm-v7
+    dl $ver linux arm64
+    dl $ver linux ppc64le
+    dl $ver linux riscv64
+    dl $ver linux s390x
+    dl $ver windows amd64
+    dl $ver windows arm64
 }
 
-dl_ver ${1:-0.20.0}
+dl_ver ${1:-0.20.1}
